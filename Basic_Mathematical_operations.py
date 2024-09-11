f6e7d67ca97dfdbc3d61a58ebaeee5772c6531c4
Vignesh Anburose <112319221+Vigneshanburose@users.noreply.github.com>
2024-09-11 16:57:07 +0530

    Add files via upload

---
+++ Basic_Mathematical_operations.py
@@ -0,0 +1,22 @@
+# perform basic mathematical operations
+# Getting two Inputs 
+a=int(input())
+b=int(input())
+
+#performing the respective operations 
+addition = a+b
+subtraction =a-b 
+multiplication = a*b 
+division = a/b
+floor_Division= a//b
+exponentiation = a**b
+modular_Division= a % b
+
+#Displaying the outputs of all the basic operations performed
+print(" Addition of the numbers is" ,addition)
+print(" Subtraction of the numbers is", subtraction)
+print(" Multiplication of the numbers is",multiplication)
+print(" Division of the numbers is",division)
+print(" Floor Division of the numbers is",floor_Division)
+print("Exponentiation of the numbers is",exponentiation)
+print("Modular Division of the numbers is ",modular_Division)
